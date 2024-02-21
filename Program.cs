5fa8486bab268bc7dd7df6563bc3f44d42f8a605
anilgo <anil.turkmen@outlook.com>
2024-02-21 19:31:44 +0300

    Added letter Grades

--- Program.cs
+++ Program.cs
@@ -38,13 +38,48 @@ foreach (string name in studentNames)
             sumAssignmentScores += score;
         }
         currentStudentGrade = (decimal)sumAssignmentScores / currentAssignments;
+        string currentStudentLetterGrade = "";
+        if (currentStudentGrade >= 97)
+            currentStudentLetterGrade = "A+";
 
+        else if (currentStudentGrade >= 93)
+            currentStudentLetterGrade = "A";
 
-        Console.WriteLine(currentStudent + ":\t\t" + currentStudentGrade + "\tA-");
+        else if (currentStudentGrade >= 90)
+            currentStudentLetterGrade = "A-";
+
+        else if (currentStudentGrade >= 87)
+            currentStudentLetterGrade = "B+";
+
+        else if (currentStudentGrade >= 83)
+            currentStudentLetterGrade = "B";
+
+        else if (currentStudentGrade >= 80)
+            currentStudentLetterGrade = "B-";
+
+        else if (currentStudentGrade >= 77)
+            currentStudentLetterGrade = "C+";
+
+        else if (currentStudentGrade >= 73)
+            currentStudentLetterGrade = "C";
+
+        else if (currentStudentGrade >= 70)
+            currentStudentLetterGrade = "C-";
+
+        else if (currentStudentGrade >= 67)
+            currentStudentLetterGrade = "D+";
+
+        else if (currentStudentGrade >= 63)
+            currentStudentLetterGrade = "D";
+
+        else if (currentStudentGrade >= 60)
+            currentStudentLetterGrade = "D-";
+        else
+            currentStudentLetterGrade = "F";
+
+        Console.WriteLine(currentStudent + ":\t\t" + currentStudentGrade + "\t" + currentStudentLetterGrade);
     }
 }
-
-
 
 Console.WriteLine("Press the Enter key to continue");
 Console.ReadLine();

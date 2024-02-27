5547738332f175c6a14a69dac117c303419a02ac
anilgo <anil.turkmen@outlook.com>
2024-02-27 23:44:15 +0300

    Boolean Expressions

---
+++ Evaluate Boolean expressions to make desicions/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.XPath;
+
+string permission = "Manager";
+int level = 30  ;
+
+
+
+string superAdmin = "Welcome, Super Admin user.";
+string admin = "Welcome, Admin user.";
+string highManager = "Contact an Admin for access.";
+string noRights = "You do not have sufficient privileges.";
+
+var result = (permission.ToLower().Trim().Contains("admin")) && level >= 55 ? superAdmin :
+                ((permission.ToLower().Trim().Contains("admin")) && level < 55) ? admin :
+                    ((permission.ToLower().Trim().Contains("manager")) && level > 20 ? highManager : noRights);
+Console.WriteLine(result);

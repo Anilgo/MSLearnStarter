b2e34c281afb27bd4918ac6e1e57850224045eec
anilgo <46276209+Anilgo@users.noreply.github.com>
2024-02-29 00:56:22 +0300

    for and for each together

--- IterateWithForLoop/Program.cs
+++ IterateWithForLoop/Program.cs
@@ -10,8 +10,28 @@ for (int i = 0; i < 10; i++)
     if (i == 7) break;
 }
 
+// string[] names = { "Alex", "Eddie", "David", "Michael" };
+// for (int i = names.Length - 1; i >= 0; i--)
+// {
+//     Console.WriteLine(names[i]);
+// }
+
+// for (int i = 0; i < 10; i += 3)
+// {
+//     Console.WriteLine(i);
+// }
+
 string[] names = { "Alex", "Eddie", "David", "Michael" };
-for (int i = names.Length - 1; i >= 0; i--)
+
+for (int i = 0; i < names.Length; i++)
 {
-    Console.WriteLine(names[i]);
+    if (names[i] == "David")
+    {
+        names[i] = "Sammy";
+    }
+}
+
+foreach (var name in names)
+{
+    Console.WriteLine(name);
 }

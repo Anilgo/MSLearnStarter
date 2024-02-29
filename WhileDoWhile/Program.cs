2bef61a981305e7badc89e4091d9d504f09b145a
anilgo <46276209+Anilgo@users.noreply.github.com>
2024-03-01 00:47:36 +0300

    while example

--- WhileDoWhile/Program.cs
+++ WhileDoWhile/Program.cs
@@ -1,9 +1,18 @@
 ﻿Random random = new Random();
 
-int numberToGuess = 0;
+// int numberToGuess = 0;
 
-do {
-    numberToGuess = random.Next(10);
-    Console.WriteLine(numberToGuess);
+// do {
+//     numberToGuess = random.Next(10);
+//     Console.WriteLine(numberToGuess);
+// }
+// while (numberToGuess != 7);
+
+int conditionNumber = random.Next(15);
+
+while (conditionNumber >= 4)
+{
+    Console.WriteLine(conditionNumber);
+    conditionNumber = random.Next(15);
+
 }
-while (numberToGuess != 7);

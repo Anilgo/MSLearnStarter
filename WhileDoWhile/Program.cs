e72317d2b13c32b683611114ac9efc66fd5f82cb
anilgo <46276209+Anilgo@users.noreply.github.com>
2024-03-01 01:17:19 +0300

    Do While RPG Project

--- WhileDoWhile/Program.cs
+++ WhileDoWhile/Program.cs
@@ -1,4 +1,4 @@
-﻿Random random = new Random();
+﻿// Random random = new Random();
 
 // int numberToGuess = 0;
 
@@ -8,11 +8,49 @@
 // }
 // while (numberToGuess != 7);
 
-int conditionNumber = random.Next(15);
+// int conditionNumber = random.Next(15);
 
-while (conditionNumber >= 4)
-{
-    Console.WriteLine(conditionNumber);
-    conditionNumber = random.Next(15);
+// while (conditionNumber >= 4)
+// {
+//     Console.WriteLine(conditionNumber);
+//     conditionNumber = random.Next(15);
 
-}
+// }
+
+
+// int current = random.Next(1, 11);
+
+// do
+// {
+//     current = random.Next(1, 11);
+
+//     if (current >= 8) continue;
+
+//     Console.WriteLine(current);
+// } while (current != 7);
+
+
+int heroHealth = 10;
+int monsterHealth = 10;
+int damage = 0;
+Random random = new Random();
+
+do
+{   
+    damage = random.Next(10);
+    monsterHealth = monsterHealth - damage;
+    Console.WriteLine($"Monster was damaged and lost {damage} health and now has {monsterHealth} health.");
+
+    if (monsterHealth <=1) continue;
+    
+    damage = random.Next(10);
+    heroHealth = heroHealth - damage;
+    Console.WriteLine($"Hero was damaged and lost {damage} health and now has {heroHealth} health.");
+
+    if (heroHealth <= 1) continue;
+    
+    
+
+} while (heroHealth >= 1 && monsterHealth >= 1);
+
+Console.WriteLine(heroHealth > monsterHealth ? "Hero Wins!" : "Monster Wins!");

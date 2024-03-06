83f8c3596d2a4406cab51bf565c4f7c989798d57
anilgo <46276209+Anilgo@users.noreply.github.com>
2024-03-06 16:01:58 +0300

    FizzBuzz

--- WhileDoWhile/Program.cs
+++ WhileDoWhile/Program.cs
@@ -30,27 +30,80 @@
 // } while (current != 7);
 
 
-int heroHealth = 10;
-int monsterHealth = 10;
-int damage = 0;
-Random random = new Random();
+// int heroHealth = 10;
+// int monsterHealth = 10;
+// int damage = 0;
+// Random random = new Random();
+
+// do
+// {
+//     damage = random.Next(10);
+//     monsterHealth = monsterHealth - damage;
+//     Console.WriteLine($"Monster was damaged and lost {damage} health and now has {monsterHealth} health.");
+
+//     if (monsterHealth <= 1) continue;
+
+//     damage = random.Next(10);
+//     heroHealth = heroHealth - damage;
+//     Console.WriteLine($"Hero was damaged and lost {damage} health and now has {heroHealth} health.");
+
+//     if (heroHealth <= 1) continue;
+
+
+
+// } while (heroHealth >= 1 && monsterHealth >= 1);
+
+// Console.WriteLine(heroHealth > monsterHealth ? "Hero Wins!" : "Monster Wins!");
+
+
+// int[] arr = [1, 2, 3, 4, 5, 6];
+
+// foreach (int value in arr)
+// {
+//     Console.WriteLine(value+10);
+//     if (value % 2 == 1) break;
+//     Console.WriteLine(value);
+// }
+
+
+// string? readResult;
+// Console.WriteLine("Enter a string:");
+// do
+// {
+//     readResult = Console.ReadLine();
+// } while (readResult == null);
+// Console.WriteLine(readResult);
+
+// string? readResult;
+// bool validEntry = false;
+// Console.WriteLine("Enter a string containing at least three characters:");
+// do
+// {
+//     readResult = Console.ReadLine();
+//     if (readResult != null)
+//     {
+//         if (readResult.Length >= 3)
+//         {
+//             validEntry = true;
+//         }
+//         else
+//         {
+//             Console.WriteLine("Your input is invalid, please try again.");
+//         }
+//     }
+// } while (validEntry == false);
+
+
+bool validEntry = false;
 
 do
-{   
-    damage = random.Next(10);
-    monsterHealth = monsterHealth - damage;
-    Console.WriteLine($"Monster was damaged and lost {damage} health and now has {monsterHealth} health.");
+{ 
+    string? input = Console.ReadLine();
+    if (input >= 5 && input <=10)
+    {
+        validEntry = true;
+    }
+    else Console.WriteLine($"Wrong input {input}");
 
-    if (monsterHealth <=1) continue;
-    
-    damage = random.Next(10);
-    heroHealth = heroHealth - damage;
-    Console.WriteLine($"Hero was damaged and lost {damage} health and now has {heroHealth} health.");
+} while (validEntry == false );
 
-    if (heroHealth <= 1) continue;
-    
-    
-
-} while (heroHealth >= 1 && monsterHealth >= 1);
-
-Console.WriteLine(heroHealth > monsterHealth ? "Hero Wins!" : "Monster Wins!");
